456b8d9d433a87526bd7f64fd48851c6875c6c76
Björn Veit <dev@bjoernveit.com>
2020-03-24 13:48:18 +0100

    feat: score now linked to cards

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,8 @@ export default {
         {word: "Agent", type: "BLUE_AGENT", state: "UNREVEALED"},
         {word: "Agent", type: "RED_AGENT", state: "UNREVEALED"},
         {word: "Agent", type: "RED_AGENT", state: "UNREVEALED"},
-        {word: "Civilist", type: "CIVILIST", state: "UNREVEALED"},
-        {word: "Civilist", type: "CIVILIST", state: "UNREVEALED"},
+        {word: "Agent", type: "BLUE_AGENT", state: "UNREVEALED"},
+        {word: "Agent", type: "BLUE_AGENT", state: "UNREVEALED"},
         {word: "Civilist", type: "CIVILIST", state: "UNREVEALED"},
         {word: "Assassin", type: "ASSASSIN", state: "UNREVEALED"},
         {word: "Agent", type: "RED_AGENT", state: "UNREVEALED"},
@@ -59,19 +59,33 @@ export default {
         {word: "Agent", type: "RED_AGENT", state: "UNREVEALED"},
       ],
       info: {label: "Hint:", text: "Vue Rocks 9"},
-      score: {
-        red: {
-          found: 5,
-          total: 7
-        },
-        blue: {
-          found: 3,
-          total: 8
-        }
-      },
       time: "0:35"
     }
-  }
+  },
+  computed: {
+    score: function() {
+      let red_total = 0;
+      let red_revealed = 0;
+      let blue_total = 0;
+      let blue_revealed = 0;
+      this.cards.forEach( card => {
+        red_total = red_total + ((card.type === "RED_AGENT") ? 1 : 0);
+        blue_total = blue_total + ((card.type === "BLUE_AGENT") ? 1 : 0);
+        red_revealed = red_revealed + ((card.type === "RED_AGENT" && card.state === "REVEALED") ? 1 : 0);
+        blue_revealed = blue_revealed + ((card.type === "BLUE_AGENT" && card.state === "REVEALED") ? 1 : 0); 
+      });
+      return {
+        red: {
+          found: red_revealed,
+          total: red_total
+        },
+        blue: {
+          found: blue_revealed,
+          total: blue_total
+        }
+      }
+    }
+  },
 }
 </script>
 
